6b7fb7f4b5462c161e9a1ceaeb68d04fba9ef34f
Pokkkk <c6369@naver.com>
2016-02-11 18:11:05 +0900

    Http연결 성공

--- app/src/main/java/com/example/bit_user/bitchatting/LoginActivity.java
+++ app/src/main/java/com/example/bit_user/bitchatting/LoginActivity.java
@@ -1,12 +1,22 @@
 package com.example.bit_user.bitchatting;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i("start", "start");
 
         id = (AutoCompleteTextView) findViewById(R.id.login_edtText1);
         password = (EditText) findViewById(R.id.login_edtText2);
@@ -27,23 +38,66 @@ public class LoginActivity extends AppCompatActivity {
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent mainSrc = new Intent(getApplicationContext(),
-                        MainsrcActivity.class);
-                startActivity(mainSrc);
+
+
+                new Thread(){
+                    @Override
+                    public void run() {
+                        HttpURLConnection conn = null;
+                        try {
+                            URL url = new URL("http://192.168.1.35/BitTalkServer/login.jsp"); //요청 URL을 입력
+                            conn = (HttpURLConnection) url.openConnection();
+
+
+                            conn.setRequestMethod("POST"); //요청 방식을 설정 (default : GET)
+
+                            conn.setDoInput(true); //input을 사용하도록 설정 (default : true)
+                            conn.setDoOutput(true); //output을 사용하도록 설정 (default : false)
+
+                            conn.setConnectTimeout(60); //타임아웃 시간 설정 (default : 무한대기)
+
+                            OutputStream os = conn.getOutputStream();
+                            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8")); //캐릭터셋 설정
+
+                            writer.write("mid=" + id.getText().toString() + "&mpasswd=" + password.getText().toString()); //요청 파라미터를 입력
+                            writer.flush();
+                            writer.close();
+                            os.close();
+
+                            conn.connect();
+
+                            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")); //캐릭터셋 설정
+
+                            StringBuilder sb = new StringBuilder();
+                            String line = null;
+                            while ((line = br.readLine()) != null) {
+                                if(sb.length() > 0) {
+                                    sb.append("\n");
+                                }
+                                sb.append(line);
+                            }
+                            JSONObject responseJSON = new JSONObject(sb.toString());
+
+
+                            System.out.println("response:" + responseJSON.toString());
+                            System.out.println((String)responseJSON.get("result"));
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            if(conn != null) {
+                                conn.disconnect();
+                            }
+                        }
+
+
+                    }
+                }.start();
+
 
             }
         });
-        Button mJoinInButton = (Button)findViewById(R.id.login_btn2);
-        mJoinInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent join = new Intent(getApplicationContext(),
-                        JoinActivity.class);
-                startActivity(join);
-
-            }
-        });
-
     }
+
+
 }
