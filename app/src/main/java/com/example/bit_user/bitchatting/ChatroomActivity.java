d648de5c71110068ebf946b975d720f1af42a16a
Patrick Peter Lee Larry Ellison <ancientgratin@gmail.com>
2016-02-12 18:00:43 +0900

    Update ChatroomActivity.java

--- app/src/main/java/com/example/bit_user/bitchatting/ChatroomActivity.java
+++ app/src/main/java/com/example/bit_user/bitchatting/ChatroomActivity.java
@@ -1,13 +1,8 @@
 package com.example.bit_user.bitchatting;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -20,11 +15,40 @@ public class ChatroomActivity extends Activity {
 
     InputMethodManager imm;     // 키보드 화면 제어를 위한 변수
 
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatroom);
         setTitle("채팅방");
 
+        // MainchatActivity에서 ChatRoom 객체가 넘어왔다고 가정하고 테스트
+        ChatRoom chatRoom = new ChatRoom(0, 1, 2, "Barack Obama", "Hello, good afternoon");
+
+        ListView lvChatRoom = (ListView)findViewById(R.id.chatroom_lvChatRoom);
+        ListView lvChatMsg = (ListView)findViewById(R.id.chatroom_lvChatMsg);
+
+        // 채팅방 제목 리스트뷰에 내용 입력
+        // MainchatAdapter 객체도 MainchatActivity에서 넘어와야 하지만 테스트를 위해 직접 만듦
+        ArrayList<MainchatLvitem> dataMainChat = new ArrayList<>();
+        MainchatLvitem mainchatLvitem = new MainchatLvitem(chatRoom.getCrName(), "");
+        dataMainChat.add(mainchatLvitem);
+        final MainchatAdapter adapterMainChat = new MainchatAdapter(this, R.layout.mainchat_listviewitem, dataMainChat);
+        lvChatRoom.setAdapter(adapterMainChat);
+
+        // 채팅 메시지 리스트뷰에 내용 입력
+        ChatMsg chatMsg = new ChatMsg(0, 0, 2, chatRoom.getCrName(), chatRoom.getLastMsg()/*, new java.util.Date()*/);
+        LinkedList<ChatroomLvitem> dataChatMsg = new LinkedList<>();
+        ChatroomLvitem chatroomLvitem = new ChatroomLvitem(chatMsg);
+        dataChatMsg.add(chatroomLvitem);
+        // 여기까지는 정상 동작 --------------------------------------------------------------------
+
+        //final ChatmsgAdapter adapterChatMsg = new ChatmsgAdapter(this, R.layout.chatroom_listviewitem, dataChatMsg);
+        //lvChatMsg.setAdapter(adapterChatMsg);
+
+
+
+
+        /*
         final ArrayList<LinkedList<String>> chatMsgList = new ArrayList<>();
         final ArrayList<String> chatRoomList = new ArrayList<>();
         final EditText edtItem = (EditText) findViewById(R.id.chatroom_edtvMsg);
@@ -59,8 +83,11 @@ public class ChatroomActivity extends Activity {
                 imm.hideSoftInputFromWindow(edtItem.getWindowToken(), 0);
             }
         });
-
+        */
     }
 }
 
 
+
+
+//http://developer.android.com/intl/ko/tools/debugging/debugging-studio.html
