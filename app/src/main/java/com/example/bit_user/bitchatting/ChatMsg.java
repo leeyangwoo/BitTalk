55abcdc568f491d6d54c5ee51419f839c6a99d97
Patrick Peter Lee Larry Ellison <ancientgratin@gmail.com>
2016-02-12 17:59:46 +0900

    Create ChatMsg.java

---
+++ app/src/main/java/com/example/bit_user/bitchatting/ChatMsg.java
@@ -0,0 +1,43 @@
+package com.example.bit_user.bitchatting;
+
+/**
+ * Created by bit-user on 2016-02-12.
+ */
+public class ChatMsg {
+    private int cmno;   // 일련번호
+    private int crno;   // 채팅방 번호
+    private int senderNo;   // 작성자 일련번호
+    private String senderName;  // 작성자명
+    private String message;     // 작성 메시지
+    //private Date sentDate;      // 작성일시
+
+    public ChatMsg(int cmno, int crno, int senderNo, String senderName, String message/*, Date sentDate*/) {
+        this.cmno = cmno;
+        this.crno = crno;
+        this.senderNo = senderNo;
+        this.senderName = senderName;
+        this.message = message;
+        //this.sentDate = sentDate;
+    }
+
+    public ChatMsg(ChatMsg chatMsg) {
+        this.cmno = chatMsg.getCmno();
+        this.crno = chatMsg.getCrno();
+        this.senderNo = chatMsg.getSenderNo();
+        this.senderName = chatMsg.getSenderName();
+        this.message = chatMsg.getMessage();
+    }
+
+    public int getCmno() { return cmno; }
+    public int getCrno() { return crno; }
+    public int getSenderNo() { return senderNo; }
+    public String getSenderName() { return senderName; }
+    public String getMessage() { return message; }
+    //public Date getSentDate() { return sentDate; }
+    public void setCmno(int cmno) { this.cmno = cmno; }
+    public void setCrno(int crno) { this.crno = crno; }
+    public void setSenderNo(int senderNo) { this.senderNo = senderNo; }
+    public void setSenderName(String senderName) { this.senderName = senderName; }
+    public void setMessage(String message) { this.message = message; }
+    //public void setSentDate(Date sentDate) { this.sentDate = sentDate; }
+}
