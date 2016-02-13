d63ada82738e20eec87b6d13f8adcf0d8ebcde0f
Patrick Peter Lee Larry Ellison <ancientgratin@gmail.com>
2016-02-13 17:27:18 +0900

    Update MainchatLvitem.java

--- app/src/main/java/com/example/bit_user/bitchatting/MainchatLvitem.java
+++ app/src/main/java/com/example/bit_user/bitchatting/MainchatLvitem.java
@@ -5,17 +5,59 @@ package com.example.bit_user.bitchatting;
  */
 public class MainchatLvitem {
 
+    // Fields
+    private static ChatMember member;
     private String crName;
     private String lastMsg;
+    private ChatRoom chatroomInstance;
 
-    public MainchatLvitem(String crName, String lastMsg) {
-        this.crName = crName;
-        this.lastMsg = lastMsg;
+    // Constructor
+    public MainchatLvitem(ChatMember member, ChatRoom chatroomInstance) {
+
+        this.member = new ChatMember(member.getMno(), member.getMid(), member.getMname());
+        try {
+            if(!memberInChatroom(chatroomInstance))
+                throw new Exception();
+            this.chatroomInstance = new ChatRoom(
+                    chatroomInstance.getCrno(), chatroomInstance.getArMno(),
+                    chatroomInstance.getParticipantNum(), chatroomInstance.getCrName(),
+                    chatroomInstance.getLastMsg());
+            this.crName = chatroomInstance.getCrName();
+            this.lastMsg = chatroomInstance.getLastMsg();
+        } catch(Exception ex) {
+
+        }
     }
 
+    // Getter/Setter
+    //   1. Getter
+    public static ChatMember getMember() { return member; }
     public String getCrName() { return crName; }
     public String getLastMsg() { return lastMsg; }
+    public ChatRoom getChatroomInstance() { return chatroomInstance; }
 
+    //   2. Setter
     public void setCrName() { this.crName = crName; }
     public void setLastMsg() { this.lastMsg = lastMsg; }
+    public void setChatroomInstance(ChatRoom chatroomInstance) {
+        try {
+            if(!memberInChatroom(chatroomInstance)) throw new Exception();
+            this.chatroomInstance = null;
+            this.chatroomInstance = new ChatRoom(
+                    chatroomInstance.getCrno(), chatroomInstance.getArMno(),
+                    chatroomInstance.getParticipantNum(), chatroomInstance.getCrName(),
+                    chatroomInstance.getLastMsg());
+        } catch(Exception ex) {
+
+        }
+    }
+
+    // 멤버 클래스가 채팅방 객체 안에 포함되어 있는지 검사
+    public static boolean memberInChatroom(ChatRoom chatroomInstance) {
+        for(int i = 0; i < chatroomInstance.getArMno().length; i++) {
+            if(member.getMno() == chatroomInstance.getArMno()[i])
+                return true;
+        }
+        return false;
+    }
 }
