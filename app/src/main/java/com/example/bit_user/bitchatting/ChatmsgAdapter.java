a705ab59913fc89909157f1741b6fd5d628c925f
AncientGratin <ancientgratin@gmail.com>
2016-02-15 09:53:34 +0900

    change ChatRoom files

--- app/src/main/java/com/example/bit_user/bitchatting/ChatmsgAdapter.java
+++ app/src/main/java/com/example/bit_user/bitchatting/ChatmsgAdapter.java
@@ -13,32 +13,40 @@ import java.util.ArrayList;
  * Created by bit-user on 2016-02-12.
  */
 public class ChatmsgAdapter extends BaseAdapter {
-    private Context mainCon;
+
     private LayoutInflater inflater;
     private ArrayList<ChatroomLvitem> arChatMsg;
     private int layout;
 
     public ChatmsgAdapter(Context mainCon, int layout, ArrayList<ChatroomLvitem> arChatMsg) {
-        this.mainCon = mainCon;
+        this.inflater = (LayoutInflater)mainCon.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.arChatMsg = arChatMsg;
         this.layout = layout;
-        this.inflater = LayoutInflater.from(mainCon);
+
     }
 
+    @Override
     public int getCount() { return arChatMsg.size(); }
-    public Object getItem(int position) { return arChatMsg.get(position).getTxtMember(); }
+
+    @Override
+    public Object getItem(int position) { return arChatMsg.get(position); }
+
+    @Override
     public long getItemId(int position) { return position; }
 
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        final int pos = position;
         if(convertView == null) {
             convertView = inflater.inflate(layout, parent, false);
         }
+
+        ChatroomLvitem lvItem = arChatMsg.get(position);
+
         // 리스트뷰 안에 넣을 아이템 변수 파일을 선언하고 초기화
         TextView txtMember = (TextView)convertView.findViewById(R.id.chatroomLv_txtMember);
         TextView txtMsg = (TextView)convertView.findViewById(R.id.chatroomLv_txtMsg);
         txtMember.setText(arChatMsg.get(position).getTxtMember());
-        txtMember.setText(arChatMsg.get(position).getTxtMsg());
+        txtMsg.setText(arChatMsg.get(position).getTxtMsg());
 
         return convertView;
     }
--- app/src/main/java/com/example/bit_user/bitchatting/ChatroomActivity.java
+++ app/src/main/java/com/example/bit_user/bitchatting/ChatroomActivity.java
@@ -21,11 +21,32 @@ public class ChatroomActivity extends Activity {
         setTitle("채팅방");
 
         // MainchatActivity에서 ChatRoom 객체가 넘어왔다고 가정하고 테스트
-        ChatRoom chatRoom = new ChatRoom(0, 1, 2, "Barack Obama", "Hello, good afternoon");
+        int[] arMno = {1, 2};
+        ChatRoom room = new ChatRoom(1, arMno, 2, "Barack Obama", "");
 
         ListView lvChatRoom = (ListView)findViewById(R.id.chatroom_lvChatRoom);
         ListView lvChatMsg = (ListView)findViewById(R.id.chatroom_lvChatMsg);
 
+        // 채팅방 제목 리스트뷰에 내용 입력
+        // MainchatAdapter 객체도 MainchatActivity에서 넘어와야 하지만 테스트를 위해 직접 만듦
+        ArrayList<MainchatLvitem> dataRoomTitle = new ArrayList<>();
+        ChatMember member = new ChatMember(1, "president@whitehouse.gov", "Barack Obama");
+
+        MainchatLvitem lvRoomTitle = new MainchatLvitem(member, room);
+        dataRoomTitle.add(lvRoomTitle);
+        final MainchatAdapter adapterRoomTitle = new MainchatAdapter(this, R.layout.mainchat_listviewitem, dataRoomTitle);
+        lvChatRoom.setAdapter(adapterRoomTitle);
+
+        ChatMsg message = new ChatMsg(1, 1, 2, "Barack Obama", "Hello, good afternoon");
+        ArrayList<ChatroomLvitem> dataChatroom = new ArrayList<>();
+        ChatroomLvitem lvItem = new ChatroomLvitem(room, message);
+        dataChatroom.add(lvItem);
+
+        final ChatmsgAdapter adapterChatMsg = new ChatmsgAdapter(this, R.layout.chatroom_listviewitem, dataChatroom);
+        lvChatMsg.setAdapter(adapterChatMsg);
+
+        // 이전 코드 백업(리스트뷰 내용 입력부터)
+        /*
         // 채팅방 제목 리스트뷰에 내용 입력
         // MainchatAdapter 객체도 MainchatActivity에서 넘어와야 하지만 테스트를 위해 직접 만듦
         ArrayList<MainchatLvitem> dataMainChat = new ArrayList<>();
@@ -35,7 +56,7 @@ public class ChatroomActivity extends Activity {
         lvChatRoom.setAdapter(adapterMainChat);
 
         // 채팅 메시지 리스트뷰에 내용 입력
-        ChatMsg chatMsg = new ChatMsg(0, 0, 2, chatRoom.getCrName(), chatRoom.getLastMsg()/*, new java.util.Date()*/);
+        ChatMsg chatMsg = new ChatMsg(0, 0, 2, chatRoom.getCrName(), chatRoom.getLastMsg());
         ArrayList<ChatroomLvitem> dataChatMsg = new ArrayList<>();
         ChatroomLvitem chatroomLvitem = new ChatroomLvitem(chatMsg);
         dataChatMsg.add(chatroomLvitem);
@@ -43,45 +64,6 @@ public class ChatroomActivity extends Activity {
 
         final ChatmsgAdapter adapterChatMsg = new ChatmsgAdapter(this, R.layout.chatroom_listviewitem, dataChatMsg);
         lvChatMsg.setAdapter(adapterChatMsg);
-
-
-
-
-        /*
-        final ArrayList<LinkedList<String>> chatMsgList = new ArrayList<>();
-        final ArrayList<String> chatRoomList = new ArrayList<>();
-        final EditText edtItem = (EditText) findViewById(R.id.chatroom_edtvMsg);
-        imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-
-        // 테스트용 채팅방 생성
-        chatRoomList.add("시민은행");
-
-        chatMsgList.add(new LinkedList<String>(){});
-        chatMsgList.get(0).add("[시민은행]당신의 개인정보가 도용되었습니다. 이를 해결하기 위해 주민등록번호와 계좌번호, 비밀번호가 필요합니다.");
-
-        ListView lvChatRoom = (ListView)findViewById(R.id.chatroom_lvChatRoom);
-        ListView lvChatMsg = (ListView)findViewById(R.id.chatroom_lvChatMsg);
-        Button btnSendMsg = (Button)findViewById(R.id.chatroom_btnSubmit);
-
-
-        final ArrayAdapter<String> chatMsgAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, chatMsgList.get(0));
-        final ArrayAdapter<String> chatRoomAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, chatRoomList);
-
-        lvChatRoom.setAdapter(chatRoomAdapter);
-        lvChatMsg.setAdapter(chatMsgAdapter);
-
-        // 버튼을 누르면 에딧텍스트에 입력된 문자열을 리스트뷰에 올린다.
-        btnSendMsg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chatMsgList.get(0).add("[나]" + edtItem.getText().toString());
-                chatMsgAdapter.notifyDataSetChanged();
-                edtItem.setText("");
-                imm.hideSoftInputFromWindow(edtItem.getWindowToken(), 0);
-            }
-        });
         */
     }
 }
