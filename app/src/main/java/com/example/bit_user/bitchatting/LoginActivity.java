bc04aca1948097a68b580998df27c6eb6c3bcfb8
leeyangwoo <lalunamaki@gmail.com>
2016-02-16 11:56:18 +0900

    로그인 액티비티 - asynctask 추가

--- app/src/main/java/com/example/bit_user/bitchatting/LoginActivity.java
+++ app/src/main/java/com/example/bit_user/bitchatting/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.bit_user.bitchatting;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -45,104 +47,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-
-                new Thread(){
-                    @Override
-                    public void run() {
-                        HttpURLConnection conn = null;
-                        try {
-                            URL url = new URL("http://192.168.1.35/BitTalkServer/login.jsp"); //요청 URL을 입력
-                            conn = (HttpURLConnection) url.openConnection();
-
-
-                            conn.setRequestMethod("POST"); //요청 방식을 설정 (default : GET)
-
-                            conn.setDoInput(true); //input을 사용하도록 설정 (default : true)
-                            conn.setDoOutput(true); //output을 사용하도록 설정 (default : false)
-
-                            conn.setConnectTimeout(1000); //타임아웃 시간 설정 (default : 무한대기)
-
-                            OutputStream os = conn.getOutputStream();
-                            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8")); //캐릭터셋 설정
-
-                            writer.write("mid=" + id.getText().toString() + "&mpasswd=" + password.getText().toString()); //요청 파라미터를 입력
-                            writer.flush();
-                            writer.close();
-                            os.close();
-
-                            conn.connect();
-
-                            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")); //캐릭터셋 설정
-
-                            StringBuilder sb = new StringBuilder();
-                            String line = null;
-                            while ((line = br.readLine()) != null) {
-                                if(sb.length() > 0) {
-                                    sb.append("\n");
-                                }
-                                sb.append(line);
-                            }
-                            responseJSON = new JSONObject(sb.toString());
-
-
-                            //Can't create handler inside thread that has not called Looper.prepare()
-                            //Non UI Thread 에서는 UI작업을 할수 없기에 요렇게 추가해서 해줘야함
-                            Handler h = new Handler(Looper.getMainLooper());
-                            h.post(new Runnable() {
-                                public void run() {
-                                    try {
-
-
-                                        if(responseJSON.get("result").equals("success")){
-
-                                            Toast.makeText(getApplicationContext(), "Successfully Logged In!",
-                                                    Toast.LENGTH_SHORT).show();
-
-                                            DatabaseHandler db = new DatabaseHandler(getApplicationContext());
-
-                                            //SQLite로 DB를 세션처럼 들고다니기 위해 테이블을 전부 리셋시켜서 한개만 생성.
-                                            db.resetTables();
-
-                                            //유저를 한명 추가해서 세션처럼 사용함.
-                                            db.addUser((responseJSON.getJSONObject("member").get("mno")).toString(),
-                                                    (responseJSON.getJSONObject("member").get("mid")).toString(),
-                                                    (responseJSON.getJSONObject("member").get("mpasswd")).toString(),
-                                                    (responseJSON.getJSONObject("member").get("mname")).toString());
-
-                                            //확인차 콘솔창에서 출력.
-                                            System.out.println(db.getUserDetails().values());
-
-                                            Intent myIntent = new Intent(getApplicationContext(),MainchatActivity.class);
-                                            startActivity(myIntent);
-
-                                        }else{
-                                            Toast.makeText(getApplicationContext(), "Incorrect Id or Password",
-                                                    Toast.LENGTH_SHORT).show();
-                                            id.setText("");
-                                            password.setText("");
-                                            id.requestFocus();
-                                        }
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-
-                                }
-                            });
-
-
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        } finally {
-                            if(conn != null) {
-                                conn.disconnect();
-                            }
-                        }
-
-
-                    }
-                }.start();
-
+                LoginTask loginTask = new LoginTask();
+                loginTask.execute(id.getText().toString(), password.getText().toString());
 
             }
         });
@@ -154,6 +60,103 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    class LoginTask extends AsyncTask<String, String, Void>{
+        protected Void doInBackground(String... param){
+            HttpURLConnection conn = null;
+            try {
+                URL url = new URL("http://192.168.1.35/BitTalkServer/login.jsp"); //요청 URL을 입력
+                conn = (HttpURLConnection) url.openConnection();
+
+
+                conn.setRequestMethod("POST"); //요청 방식을 설정 (default : GET)
+
+                conn.setDoInput(true); //input을 사용하도록 설정 (default : true)
+                conn.setDoOutput(true); //output을 사용하도록 설정 (default : false)
+
+                conn.setConnectTimeout(1000); //타임아웃 시간 설정 (default : 무한대기)
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8")); //캐릭터셋 설정
+
+                writer.write("mid=" + param[0] + "&mpasswd=" + param[1]); //요청 파라미터를 입력
+                writer.flush();
+                writer.close();
+                os.close();
+                Log.i("login",param[0]+param[1]);
+
+                conn.connect();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")); //캐릭터셋 설정
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    if (sb.length() > 0) {
+                        sb.append("\n");
+                    }
+                    sb.append(line);
+                }
+                responseJSON = new JSONObject(sb.toString());
+
+                Handler h = new Handler(Looper.getMainLooper());
+                h.post(new Runnable() {
+                    public void run() {
+                        try {
+
+
+                            if(responseJSON.get("result").equals("success")){
+
+                                Toast.makeText(getApplicationContext(), "Successfully Logged In!",
+                                        Toast.LENGTH_SHORT).show();
+
+                                DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+
+                                db.resetTables();
+
+
+                                //유저를 한명 추가해서 세션처럼 사용함.
+                                db.addUser((responseJSON.getJSONObject("member").get("mno")).toString(),
+                                        (responseJSON.getJSONObject("member").get("mid")).toString(),
+                                        (responseJSON.getJSONObject("member").get("mpasswd")).toString(),
+                                        (responseJSON.getJSONObject("member").get("mname")).toString());
+
+                                //확인차 콘솔창에서 출력.
+                                System.out.println(db.getUserDetails().values());
+
+                                Intent myIntent = new Intent(getApplicationContext(),MainchatActivity.class);
+                                startActivity(myIntent);
+
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Incorrect Id or Password",
+                                        Toast.LENGTH_SHORT).show();
+                                id.setText("");
+                                password.setText("");
+                                id.requestFocus();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if(conn != null) {
+                    conn.disconnect();
+                }
+            }
+            return null;
+        }
+        protected void onPostExecute(Void result){
+            super.onPostExecute(result);
+            Log.i("Post","Post");
+
+        }
     }
 
 
