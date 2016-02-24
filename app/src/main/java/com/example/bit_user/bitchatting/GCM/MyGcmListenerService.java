5e7a549f49ffe2565b5ff880fb8747f4cd2f04b4
leeyangwoo <lalunamaki@gmail.com>
2016-02-24 14:51:40 +0900

    icon

--- app/src/main/java/com/example/bit_user/bitchatting/GCM/MyGcmListenerService.java
+++ app/src/main/java/com/example/bit_user/bitchatting/GCM/MyGcmListenerService.java
@@ -53,7 +53,7 @@ public class MyGcmListenerService extends GcmListenerService {
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setSmallIcon(R.drawable.talk)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setAutoCancel(true)
